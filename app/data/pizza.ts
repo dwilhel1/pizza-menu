bc6f7765e4a31947d90ce9c259aed79f1fab359d
Douglas Wilhelm <douglas.p.wilhelm@gmail.com>
2026-05-06 21:26:24 -0500

    Add hardcoded pizza data and Pizza type

---
+++ app/data/pizza.ts
@@ -0,0 +1,8 @@
+import { Pizza } from "@/app/types";
+
+export const pizzas: Pizza[] = [
+    { id: 1, name: 'Margherita', description: 'Classic tomato and mozzarella', price: 12 },
+    { id: 2, name: 'Pepperoni', description: 'Loaded with pepperoni', price: 14 },
+    { id: 3, name: 'BBQ Chicken', description: 'Smoky BBQ sauce, chicken, red onion', price: 15 },
+    { id: 4, name: 'Veggie', description: 'Bell peppers, mushrooms, olives', price: 13 },
+];
